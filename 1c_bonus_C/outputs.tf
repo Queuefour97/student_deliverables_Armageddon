25a42c79a788a09298a06a4f4abfbd93e6c13144
Queuefour97 <107631310+Queuefour97@users.noreply.github.com>
2026-02-02 10:31:15 -0500

    Add files via upload

---
+++ 1c_bonus_C/outputs.tf
@@ -0,0 +1,71 @@
+# Explanation: Outputs are your mission report—what got built and where to find it.
+output "armageddon_vpc_id" {
+  value = aws_vpc.armageddon_vpc01.id
+}
+
+output "armageddon_public_subnet_ids" {
+  value = aws_subnet.armageddon_public_subnets[*].id
+}
+
+output "armageddon_private_subnet_ids" {
+  value = aws_subnet.armageddon_private_subnets[*].id
+}
+
+output "armageddon_ec2_instance_id" {
+  value = aws_instance.armageddon_ec201.id
+}
+
+output "armageddon_rds_endpoint" {
+  value = aws_db_instance.armageddon_rds01.address
+}
+
+output "armageddon_sns_topic_arn" {
+  value = aws_sns_topic.armageddon_sns_topic01.arn
+}
+
+output "armageddon_log_group_name" {
+  value = aws_cloudwatch_log_group.armageddon_log_group01.name
+}
+
+output "armageddon_route53_zone_id" {
+  value = local.armageddon_zone_id
+}
+
+output "armageddon_app_url_https" {
+  value = "https://${var.app_subdomain}.${var.domain_name}"
+}
+#################################################
+# output "instance_id" {
+#   value = aws_instance.armageddon_ec201.id
+# }
+
+# #Bonus-A outputs (append to outputs.tf)
+
+# # Explanation: These outputs prove Chewbacca built private hyperspace lanes (endpoints) instead of public chaos.
+# output "armageddon_vpce_ssm_id" {
+#   value = aws_vpc_endpoint.armageddon_vpce_ssm01.id
+# }
+
+# output "armageddon_vpce_logs_id" {
+#   value = aws_vpc_endpoint.armageddon_vpce_logs01.id
+# }
+
+# output "armageddon_vpce_secrets_id" {
+#   value = aws_vpc_endpoint.armageddon_vpce_secrets01.id
+# }
+
+# output "armageddon_vpce_s3_id" {
+#   value = aws_vpc_endpoint.armageddon_vpce_s3_gw01.id
+# }
+
+# output "armageddon_private_ec2_instance_id_bonus" {
+#   value = aws_instance.armageddon_ec201_private_bonus.id
+# }
+
+# output "armageddon_vpce_ec2messages_id" {
+#   value = aws_vpc_endpoint.armageddon_vpce_ec2messages01.id
+# }
+
+# output "armageddon_vpce_ssmmessages_id" {
+#   value = aws_vpc_endpoint.armageddon_vpce_ssmmessages01.id
+# }
